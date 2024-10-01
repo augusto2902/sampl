c5a3e49953c6be59b49ee1d04197d118c7ebb53c
augusto <augustobruno2902@gmail.com>
2024-10-01 20:45:10 -0300

    s

---
+++ EmployeeCrudApi/EmployeeCrudApi/Controllers/EmployeeController.cs
@@ -0,0 +1,59 @@
+using EmployeeCrudApi.Data;
+using EmployeeCrudApi.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmployeeCrudApi.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class EmployeeController : ControllerBase
+    {
+        private ApplicationDbContext _context;
+
+        public EmployeeController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<List<Employee>> GetAll()
+        {
+            return await _context.Employees.ToListAsync();
+        }
+
+        [HttpGet]
+        public async Task<Employee> GetById(int id)
+        {
+            return await _context.Employees.FindAsync(id);
+        }
+
+        [HttpPost]
+        public async Task Create([FromBody] Employee employee)
+        {
+            employee.CreatedDate = DateTime.Now;
+            await _context.Employees.AddAsync(employee);
+            await _context.SaveChangesAsync();
+        }
+
+        [HttpPut]
+        public async Task Update([FromBody] Employee employee)
+        {
+            Employee employeeToUpdate = await _context.Employees.FindAsync(employee.Id);
+            employeeToUpdate.Name = employee.Name;
+            await _context.SaveChangesAsync();
+        }
+
+        [HttpDelete]
+        public async Task Delete(int id)
+        {
+            var employeeToDelete = await _context.Employees.FindAsync(id);
+            _context.Remove(employeeToDelete);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
